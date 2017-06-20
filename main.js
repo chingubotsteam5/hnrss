42a62e141bd0597c582258a7a188ed21134e7cd4
Simon Morgan <sjm@sjm.io>
2017-06-20 21:04:29 +0100

    Factor out polling delay

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const FeedParser = require("feedparser");
 
 const feedUrl = "https://news.ycombinator.com/rss";
 
+const secondsBetweenPolls = 10;
+
 function processArticle(article) {
   console.log(article.title);
 }
@@ -12,7 +14,7 @@ function checkFeed() {
   feedparser.on("data", processArticle);
 
   const req = request(feedUrl);
-  req.on("end", () => setTimeout(checkFeed, 10000));
+  req.on("end", () => setTimeout(checkFeed, secondsBetweenPolls * 1000));
   req.pipe(feedparser);
 }
 
