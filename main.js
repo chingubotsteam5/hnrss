074e1b6ffa605a16116c334c8ed905fc22bf8673
Simon Morgan <sjm@sjm.io>
2017-06-20 20:43:42 +0100

    Implement basic persistent RSS feed checking

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
 const request = require("request");
 const FeedParser = require("feedparser");
 
-const req = request("https://news.ycombinator.com/rss");
-const feedparser = new FeedParser();
+const feedUrl = "https://news.ycombinator.com/rss";
 
-req.on("error", (error) => console.log(error));
+function processArticle(article) {
+  console.log(article.title);
+}
 
-req.on("response", function (res) {
-  var stream = this; // `this` is `req`, which is a stream
+function checkFeed() {
+  const feedparser = new FeedParser();
+  feedparser.on("data", processArticle);
 
-  if (res.statusCode !== 200) {
-    this.emit("error", new Error("Bad status code"));
-  } else {
-    stream.pipe(feedparser);
-  }
-});
+  const req = request(feedUrl);
+  req.on("end", () => setTimeout(checkFeed, 10000));
+  req.pipe(feedparser);
+  // request(feedUrl).pipe(feedparser);
+}
 
-feedparser.on("error", (error) => console.log(error));
-
-feedparser.on("readable", function () {
-  var stream = this; // `this` is `feedparser`, which is a stream
-  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-  var item;
-
-  while (item = stream.read()) {
-    console.log(`${item.title}: ${item.link}`);
-  }
-});
+checkFeed();
