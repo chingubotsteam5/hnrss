d1ac18693e73e44558824a03f629fe02a7dc0769
Simon Morgan <sjm@sjm.io>
2017-06-21 10:39:07 +0100

    Save and load the list of seen articles to a JSON file

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
+const fs = require("fs");
+
 const request = require("request");
 const FeedParser = require("feedparser");
 const crc = require("crc");
 
 const feedUrl = "https://news.ycombinator.com/rss";
-
 const secondsBetweenPolls = 30;
+const seenArticlesFilename = "seen-articles.json";
 
-const seenArticles = [];
+let seenArticles = [];
+
+function loadSeenArticles() {
+  try {
+    const data = fs.readFileSync(seenArticlesFilename);
+    seenArticles = JSON.parse(data);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error(`${seenArticlesFilename} is missing. First run?`);
+    } else {
+      console.error(err);
+    }
+  }
+}
+
+function saveSeenArticles() {
+  try {
+    fs.writeFileSync(seenArticlesFilename, JSON.stringify(seenArticles));
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
 function processArticle(article) {
   const checksum = crc.crc32(article.guid).toString(16);
   if (seenArticles.indexOf(checksum) === -1) {
     seenArticles.push(checksum);
-    console.log(article.title);
+    console.log(`${article.title}: ${article.link}`);
   }
 }
 
@@ -24,5 +47,13 @@ function checkFeed() {
   req.on("end", () => setTimeout(checkFeed, secondsBetweenPolls * 1000));
   req.pipe(feedparser);
 }
+
+process.on("exit", () => saveSeenArticles);
+process.on("SIGINT", () => {
+  saveSeenArticles();
+  process.exit(1);
+});
+
+loadSeenArticles();
 
 checkFeed();
